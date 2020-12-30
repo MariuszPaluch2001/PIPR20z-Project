1a9bad00a83686310280c34e21556cb355620207
mpaluch2 <01158867@pw.edu.pl>
2020-12-30 17:07:13 +0100

    Poprawka jednego błędu

--- gracz.py
+++ gracz.py
@@ -4,7 +4,7 @@ class Gracz(object):
     def __init__(self, kolor):
         """
             Tworzy gracza
-        """ 
+        """
         self.kolor = kolor
         self.pionki = []
 
@@ -14,10 +14,10 @@ class Gracz(object):
     def wczytaj_nastepny_ruch(self, plansza, gracz):
         #statement1 = "Select one of your tokens eg. " + chr(b.whitelist[0][0]+97) + str(b.whitelist[0][1])
         print("Teraz kolej na gracza " + ( '◆' if self.kolor != 0 else '◇'))
-        
+
         while True: # Dopoki poprawna wartosc pola
             ruch = []
-        
+
             print("Lista mozliwych ruchow: ")
             plansza.wypisz_mozliwe_ruchy(gracz)
 
@@ -25,24 +25,24 @@ class Gracz(object):
             ruch.append(tuple(input().lower()))
 
             ruch_z = (int(ruch[0][1]), ord(ruch[0][0]) - 97)
-            
-      
+
+
 
             # for i in plansza.iterator_mozliwych_posuniec(ruch_z,[(-1,-1),(1,-1)],self, None):
             #     print(i)
 
-         
+
             ruch.append(tuple(input().lower()))
             if len(ruch[0]) != 2  or len(ruch[1]) != 2 :
                 print("Ups, niepoprawny ruch")
                 continue
-            
-            
+
+
             ruch_do = (int(ruch[1][1]), ord(ruch[1][0]) - 97)
-        
+
             # Is the piece we want to move one we own?
             if not (ruch_z in plansza.pionki[self.kolor]):
-                print("Ups, to nie twoj pionek, wybierz go z listy (wiersz, kolumna) " + str(plansza.pionki[self.color]))
+                print("Ups, to nie twoj pionek, wybierz go z listy (wiersz, kolumna) " + str(plansza.pionki[self.kolor]))
                 continue
             break
         move = (ruch_z, ruch_do)
